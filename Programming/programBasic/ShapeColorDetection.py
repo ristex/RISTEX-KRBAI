988f975fbd60a5e2a9608f5ed309819db7ccb17c
RISTEX URO ITB <48144851+ristex@users.noreply.github.com>
2024-01-24 13:41:08 +0700

    Add files via upload

---
+++ Programming/programBasic/ShapeColorDetection.py
@@ -0,0 +1,121 @@
+import cv2
+import numpy as np
+
+
+frameWidth = 640
+frameHeight = 480
+cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+cap.set(10,150)
+
+def empty(a):
+    pass
+
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
+
+def getContours(img):
+    contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        print(area)
+        if area>500:
+            cv2.drawContours(imgContour, cnt, -1, (255, 0, 0), 3)
+            peri = cv2.arcLength(cnt,True)
+            #print(peri)
+            approx = cv2.approxPolyDP(cnt,0.02*peri,True)
+            print(len(approx))
+            objCor = len(approx)
+            x, y, w, h = cv2.boundingRect(approx)
+
+            if objCor ==3: objectType ="Tri"
+            elif objCor == 4:
+                aspRatio = w/float(h)
+                if aspRatio >0.98 and aspRatio <1.03: objectType= "Square"
+                else:objectType="Rectangle"
+            elif objCor>4: objectType= "Circles"
+            else:objectType="None"
+
+
+
+            cv2.rectangle(imgContour,(x,y),(x+w,y+h),(0,255,0),2)
+            cv2.putText(imgContour,objectType,
+                        (x+(w//2)-10,y+(h//2)-10),cv2.FONT_HERSHEY_COMPLEX,0.7,
+                        (255,255,255),2)
+
+
+path = 'shapes.png'
+cv2.namedWindow("TrackBars")
+cv2.resizeWindow("TrackBars",640,240)
+cv2.createTrackbar("Hue Min","TrackBars",54,179,empty)
+cv2.createTrackbar("Hue Max","TrackBars",179,179,empty)
+cv2.createTrackbar("Sat Min","TrackBars",80,255,empty)
+cv2.createTrackbar("Sat Max","TrackBars",255,255,empty)
+cv2.createTrackbar("Val Min","TrackBars",0,255,empty)
+cv2.createTrackbar("Val Max","TrackBars",134,255,empty)
+
+while True:
+    success, img = cap.read()
+    #img = cv2.imread(path)
+    imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    h_min = cv2.getTrackbarPos("Hue Min","TrackBars")
+    h_max = cv2.getTrackbarPos("Hue Max", "TrackBars")
+    s_min = cv2.getTrackbarPos("Sat Min", "TrackBars")
+    s_max = cv2.getTrackbarPos("Sat Max", "TrackBars")
+    v_min = cv2.getTrackbarPos("Val Min", "TrackBars")
+    v_max = cv2.getTrackbarPos("Val Max", "TrackBars")
+    print(h_min,h_max,s_min,s_max,v_min,v_max)
+    lower = np.array([h_min,s_min,v_min])
+    upper = np.array([h_max,s_max,v_max])
+    mask = cv2.inRange(imgHSV,lower,upper)
+    imgResult = cv2.bitwise_and(img,img,mask=mask)
+
+    imgContour = imgResult.copy()
+
+    imgGray = cv2.cvtColor(imgResult,cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray,(7,7),1)
+    imgCanny = cv2.Canny(imgBlur,50,50)
+    getContours(imgCanny)
+
+    imgBlank = np.zeros_like(img)
+
+    # cv2.imshow("Original",img)
+    # cv2.imshow("HSV",imgHSV)
+    # cv2.imshow("Mask", mask)
+    # cv2.imshow("Result", imgResult)
+
+    imgStack = stackImages(0.7,([img,imgHSV,mask],[imgResult, imgContour, imgBlank]))
+    cv2.imshow("Stacked Images", imgStack)
+
+    #cv2.waitKey(1)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
