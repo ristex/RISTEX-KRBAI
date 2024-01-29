c57f313dc507143a599a31e9dbc3d33ca4109520
RISTEX URO ITB <48144851+ristex@users.noreply.github.com>
2024-01-29 15:16:04 +0700

    Add files via upload

---
+++ Programming/coba.py
@@ -0,0 +1,133 @@
+import cv2
+import numpy as np
+
+frameWidth = 640
+frameHeight = 480
+cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+cap.set(10, 150)
+
+myColors = [[5, 83, 229, 108, 255, 255],     # orange
+            [132, 58, 43, 179, 255, 255],   # merah
+            [25, 64, 114, 91, 157, 255]]    # hijau 
+myColorValues = [[0, 165, 255],
+                 [0, 0, 255],
+                 [0, 255, 0]]
+
+myPoints = []  ## [x , y , colorId ]
+
+def stackImages(scale, imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+
+    if rowsAvailable:
+        for x in range(0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape[:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y] = cv2.cvtColor(imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank] * rows
+        hor_con = [imageBlank] * rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None, scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor = np.hstack(imgArray)
+        ver = hor
+
+    return ver
+
+def findColor(img, myColors, myColorValues):
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    newPoints = []
+    for color, colorValue in zip(myColors, myColorValues):
+        lower = np.array(color[0:3])
+        upper = np.array(color[3:6])
+        mask = cv2.inRange(imgHSV, lower, upper)
+
+        x, y, objectType = getContours(mask, img, colorValue)
+        
+        if x != 0 and y != 0:
+            newPoints.append([x, y, objectType, colorValue])
+
+    return newPoints
+
+def getContours(img, imgResult, colorValue):
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    x, y = 0, 0  # Inisialisasi x dan y
+    objectType = "None"  # Inisialisasi objectType ke default value
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > 500:
+            peri = cv2.arcLength(cnt, True)
+            approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
+            x, y, w, h = cv2.boundingRect(approx)
+
+            objCor = len(approx)
+
+            if objCor == 3: 
+                objectType = "Tri"
+            elif objCor == 4:
+                aspRatio = w / float(h)
+                if aspRatio > 0.90 and aspRatio < 1.1: 
+                    objectType = "Square"
+                else:
+                    objectType = "Rectangle"
+            elif objCor > 4: 
+                objectType = "Circles"
+            else:
+                objectType = "None"
+
+            cv2.drawContours(imgResult, cnt, -1, (255, 0, 0), 3)
+            cv2.rectangle(imgResult, (x, y), (x + w, y + h), colorValue, 2)
+            cv2.putText(imgResult, objectType, (x + (w // 2) - 10, y + (h // 2) - 10),
+                        cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 0, 0), 2)
+
+            # Tampilkan koordinat pada layar
+            cv2.putText(imgResult, f"({x}, {y})", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, colorValue, 2)
+            print(f"Detected {objectType} at coordinates ({x}, {y}) with color {colorValue}")
+
+    return x, y, objectType
+
+def detectShapesAndColors(img):
+    imgContour = img.copy()
+
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 3)
+    imgCanny = cv2.Canny(imgBlur, 50, 50)
+
+    newPoints = findColor(img, myColors, myColorValues)
+
+    if len(newPoints) != 0:
+        for newP in newPoints:
+            myPoints.append(newP)
+
+    imgBlank = np.zeros_like(img)
+    imgStack = stackImages(0.6, ([img, imgGray, imgBlur],
+                                 [imgCanny, imgContour, imgBlank]))
+
+    cv2.imshow("Stack", imgStack)
+    return imgContour
+
+while True:
+    success, img = cap.read()
+    imgResult = detectShapesAndColors(img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
